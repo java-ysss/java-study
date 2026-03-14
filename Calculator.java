75a4904946b827be404f1f5e41a278a42b6ac638
Yooo <aboutface.tokyo@gmail.com>
2026-03-14 15:34:44 +0900

    電卓プログラム 履歴機能追加

--- Calculator.java
+++ Calculator.java
@@ -4,6 +4,8 @@ class Calculator {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         String[] numbers = { "1:足し算", "2:引き算", "3:掛け算", "4:割り算" };
+        String[] history = new String[100];
+        int historyIndex = 0;
 
         while (true) {
 
@@ -14,13 +16,22 @@ class Calculator {
             System.out.println(operation + "を選択しました");
 
             int calculateResult = calculate(num1, num2, operation);
-            showResult(calculateResult);
+            String result = showResult(calculateResult, num1, num2, operation);
+
+            history[historyIndex] = result;
+            historyIndex++;
 
             int finish = finishCode(scanner);
-                if (finish == 2) {
-                    break;
-                }
+            if (finish == 2) {
+                break;
+            }
 
+        }
+
+        System.out.println("計算結果");
+
+        for(int i = 0; i < historyIndex; i++){
+            System.out.println(history[i]);
         }
 
     }
@@ -38,8 +49,15 @@ class Calculator {
         for (int i = 0; i < numbers.length; i++) {
             System.out.println(numbers[i]);
         }
-        int select = scanner.nextInt();
-        return select;
+        while (true) {
+            int select = scanner.nextInt();
+            if (select >= 1 && select <= 4) {
+
+                return select;
+            }
+
+            System.out.println("1 ~ 4の間で入力してください");
+        }
     }
 
     // 計算する
@@ -64,16 +82,43 @@ class Calculator {
     }
 
     // 計算結果
-    public static void showResult(int calculateResult) {
-        System.out.println("計算結果は：" + calculateResult);
-    }
+    public static String showResult(int calculateResult, int num1, int num2, int operation) {
+        String symbol = "";
+
+        switch (operation) {
+            case 1:
+                symbol = "+";
+                break;
+            case 2:
+                symbol = "-";
+                break;
+            case 3:
+                symbol = "*";
+                break;
+            case 4:
+                symbol = "/";
+                break;
+        }
+
+        String  result = (num1 + "" + symbol + "" + num2 + " = " + calculateResult);
+      
+        System.out.println(result);
+        return result;
+        }
+    
 
     // 終了メソッド
-    public static int finishCode(Scanner scanner){
+    public static int finishCode(Scanner scanner) {
         System.out.println("もう一度計算しますか？");
         System.out.println("1:はい 2:終了");
-        int choice = scanner.nextInt();
-        return choice;
+        while (true) {
+            int choice = scanner.nextInt();
+
+            if (choice == 1 || choice == 2) {
+                return choice;
+            }
+            System.out.println("１か２で入力してください");
+        }
     }
 
 }
