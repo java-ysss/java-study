fe6ce0f72dff2b6d520cf782bd5e551ffc23a9b5
Yooo <aboutface.tokyo@gmail.com>
2026-03-18 15:53:21 +0900

    電卓改善学習

--- Calculator/MiniCalc.java
+++ Calculator/MiniCalc.java
@@ -1,39 +1,83 @@
 package Calculator;
 
 import java.util.Scanner;
+import java.util.ArrayList;
 
 class MiniCals {
     public static void main(String[] args) {
+        ArrayList<String> history = new ArrayList<>();
         Scanner scanner = new Scanner(System.in);
-        String[] numbers = { "足し算", "引き算", "掛け算" };
+        while (true) {
 
-        int num1 = getNumber(scanner);
-        int num2 = getNumber(scanner);
+            String[] numbers = { "足し算", "引き算", "掛け算", "割り算", "履歴表示", "終了" };
 
-        int selectResult = selectOperation(scanner,numbers);
+            int num1 = getNumber(scanner);
+            int num2 = getNumber(scanner);
 
-        int result = testOperation(num1, num2, selectResult);
+            int selectResult = selectOperation(scanner, numbers);
 
-        resultNumber(result);
+            if (selectResult == 5) {
+                System.out.println("計算履歴： ");
+                if (history.isEmpty()) {
+                    System.out.println("まだ履歴はありません");
+                } else {
+                    for (int i = 0; i < history.size(); i++) {
+                        System.out.println((i + 1) + " : " + history.get(i));
+                    }
+                }
+                continue;
+            }
+            if (selectResult == 6) {
+                System.out.println("プログラムを終了します");
+                break;
+            }
 
-     
+            int result = testOperation(num1, num2, selectResult);
+
+            String operator = " ";
+            switch (selectResult) {
+                case 1:
+                    operator = "+";
+                    break;
+                case 2:
+                    operator = "-";
+                    break;
+                case 3:
+                    operator = "*";
+                    break;
+                case 4:
+                    operator = "/";
+                    break;
+            }
+
+            history.add(num1 + " " + operator + " " + num2 + " = " + result);
+            resultNumber(result);
+
+        }
 
     }
 
     // 入力メソッド
     public static int getNumber(Scanner scanner) {
-        System.out.print("数字をに入力してください: ");
-        int number = scanner.nextInt();
+        System.out.print("数字を入力してください: ");
+        while (true) {
 
-        return number;
+            if (scanner.hasNextInt()) {
+                int number = scanner.nextInt();
+                return number;
+            } else {
+                System.out.println("数字を入力してください");
+                scanner.next();
+            }
+        }
     }
 
     // 計算方法選択メソッド
-    public static int selectOperation(Scanner scanner,String[] numbers) {
+    public static int selectOperation(Scanner scanner, String[] numbers) {
 
         System.out.println("計算方法を選んでください");
 
-         for (int i = 0; i < numbers.length; i++) {
+        for (int i = 0; i < numbers.length; i++) {
             System.out.println((i + 1) + ":" + numbers[i]);
         }
         int number = scanner.nextInt();
@@ -51,9 +95,15 @@ class MiniCals {
                 return num1 - num2;
             case 3:
                 return num1 * num2;
+            case 4:
+                if (num2 == 0) {
+                    System.out.println("0では割り算できません");
+                    return 0;
+                }
+                return num1 / num2;
 
             default:
-                System.out.println("1 ~ 3 を選んでください");
+                System.out.println("1 ~ 6 を選んでください");
                 return 0;
         }
     }
@@ -62,5 +112,4 @@ class MiniCals {
     public static void resultNumber(int result) {
         System.out.println("計算結果は" + result + "です");
     }
-
 }
